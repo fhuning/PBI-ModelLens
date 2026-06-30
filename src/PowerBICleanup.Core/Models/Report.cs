78d68d5e7cb7afa96ecb48691390d85484b81af4
Frank Huning <frank.huning@landal.org>
2026-06-30 20:23:31 +0200

    Add report page domain model

--- src/PowerBICleanup.Core/Models/Report.cs
+++ src/PowerBICleanup.Core/Models/Report.cs
@@ -5,4 +5,6 @@ public sealed class Report
     public required string Name { get; init; }
 
     public required string FolderPath { get; init; }
+
+    public List<ReportPage> Pages { get; } = [];
 }
--- src/PowerBICleanup.Core/Models/ReportPage.cs
+++ src/PowerBICleanup.Core/Models/ReportPage.cs
@@ -2,7 +2,7 @@ namespace PowerBICleanup.Core.Models;
 
 public sealed class ReportPage
 {
-    public required string Id { get; init; }
+    public required string Name { get; init; }
 
-    public required string DisplayName { get; init; }
+    public required string FolderPath { get; init; }
 }
